9b95971fdbfa0803b8f7a9961b5b0dcc223fe96d
milberonja <milberonja@gmail.com>
2024-06-15 20:22:20 +0200

    Adding @RefreshScope annotation to HomeController

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,10 +1,13 @@
 package com.polarbookshop.catalogservice;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope //Adding this annotation in order to this Been reload after /actuator/refresh command is triggered.
+              //This annotation is like a EventListener for Refresh event
 public class HomeController {
 
     @Value("${polar.greeting}")
