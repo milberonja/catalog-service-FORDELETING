70a36d60a935c8ada9358ee769f78a9916606bdf
milberonja <milberonja@gmail.com>
2024-06-15 19:23:37 +0200

    Added Actuator dependency in the pom.xml and added configuration in .yml file

--- src/main/java/com/polarbookshop/catalogservice/HomeController.java
+++ src/main/java/com/polarbookshop/catalogservice/HomeController.java
@@ -1,13 +1,17 @@
 package com.polarbookshop.catalogservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${polar.greeting}")
+    private String message;
+
     @GetMapping("/")
     public String getGreeting(){
-        return "Welcome to the book catlog!";
+        return message;
     }
 }
